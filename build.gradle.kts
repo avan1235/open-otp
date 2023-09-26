46d9c03e273f9c0def546ff68de977d331d0c5ad
avan1235 <macieekprocyk@gmail.com>
2023-09-26 10:02:15 +0200

    configure version catalog update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+
 plugins {
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.kotlin.parcelize) apply false
@@ -10,4 +12,20 @@ plugins {
 
     alias(libs.plugins.gradle.versions) apply true
     alias(libs.plugins.version.catalog.update) apply true
+}
+
+versionCatalogUpdate {
+    sortByKey = true
+}
+
+fun isStable(version: String): Boolean {
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
+    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+    return stableKeyword || regex.matches(version)
+}
+
+tasks.withType<DependencyUpdatesTask> {
+    rejectVersionIf {
+        !isStable(candidate.version)
+    }
 }
